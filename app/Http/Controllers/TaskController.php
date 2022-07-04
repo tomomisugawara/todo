576610edf9a12f594ba53510de1f3790df059e02
tomomisugawara <hello.tra.sugawara@gmail.com>
2022-07-04 15:02:30 +0900

    タスク一覧表示機能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task; //
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+        // すべてのフォルダを取得する
         $folders = Folder::all();
+        // dd($folder);
+        // exit;
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得する
+        // $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
-        ]);
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+        ]);;
     }
 }

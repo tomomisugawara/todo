1f5e070eccd42ff8523ca7d841201615d76c0536
tomomisugawara <hello.tra.sugawara@gmail.com>
2022-07-06 14:56:16 +0900

    アプリタスク機能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task; //
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -30,12 +31,28 @@ class TaskController extends Controller
     }
 
     /**
- * GET /folders/{id}/tasks/create
- */
-public function showCreateForm(int $id)
-{
-    return view('tasks/create', [
-        'folder_id' => $id
-    ]);
-}
+     * GET /folders/{id}/tasks/create
+     */
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
+    }
 }

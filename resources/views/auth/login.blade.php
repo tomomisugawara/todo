c39a946b5935ccf60a88944c0b386e3420f1fb18
tomomi sugawara <tomomisugawara@tomominoMacBook-Pro.local>
2022-08-17 22:09:41 +0900

    8/17

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,63 @@
 @extends('layout')
 
 @section('content')
-<div class="container">
+    <div class="container">
 
-<!-- <div class="balloon">
-    <div class="balloon login-text">
-      <h5>ちいさなことからこつこつと。</h5><br>
-      そんな思いから誕生したのがTODOアプリ「TIIKOTU」にゃ!<br>
-      たとえば、前日より５分早く起きるとか、帰ったら手を洗うとか、スリッパを揃えるとか。<br>
-      いつでも気軽につかってほしいのにゃ。<br>
-      タスクを達成するとTIIKOTUがほめてくれたり、くれなかったり、たまに独り言をつぶやいたりするのにゃ。<br>
-      TIIKOTUと一緒に楽しくすごそうにゃ!<br>
-    </div>
-  </div> -->
 
-  <div class="login_container">
-    <img class="login-neko" src="../image/neko1.png" alt="">
-    <div class="row">
-      <div class="col col-md-offset-3 col-md-6">
-        <nav class="panel panel-default">
-          <div class="panel-heading">ログイン</div>
-          <div class="panel-body">
-            @if($errors->any())
-            <div class="alert alert-danger">
-              @foreach($errors->all() as $message)
-              <p>{{ $message }}</p>
-              @endforeach
+        <div class="login-neko-wrapper">
+
+            <div class="neko">
+                <img class="login-neko" src="../image/neko1.png" alt="">
             </div>
-            @endif
-            <form action="{{ route('login') }}" method="POST">
-              @csrf
-              <div class="form-group">
-                <label for="email">メールアドレス</label>
-                <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" />
-              </div>
-              <div class="form-group">
-                <label for="password">パスワード</label>
-                <input type="password" class="form-control" id="password" name="password" />
-              </div>
-              <div class="text-right">
-                <button type="submit" class="btn btn-success"">送信</button>
-              </div>
-            </form>
-          </div>
-        </nav>
-        <div class="text-center">
-          <a href="{{ route('password.request') }}">パスワードの変更はこちらから</a>
-        </div>
-      </div>
-    </div>
-  </div>
 
-</div>
+            <div class="balloon">
+                <div class="balloon-innar">
+                    <p>吾輩はTIIKOTUにゃ。<br>
+                    ちいさなことからこつこつとできたらいい。<br>
+                    そんな思いから生まれたにゃ。<br>
+					</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="login_container">
+
+            <div class="row">
+                <div class="col col-md-offset-3 col-md-6">
+                    <nav class="panel panel-default">
+                        <div class="panel-heading">ログイン</div>
+                        <div class="panel-body">
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    @foreach ($errors->all() as $message)
+                                        <p>{{ $message }}</p>
+                                    @endforeach
+                                </div>
+                            @endif
+                            <form action="{{ route('login') }}" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="email">メールアドレス</label>
+                                    <input type="text" class="form-control" id="email" name="email"
+                                        value="{{ old('email') }}" />
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">パスワード</label>
+                                    <input type="password" class="form-control" id="password" name="password" />
+                                </div>
+                                <div class="text-right">
+                                    <button type="submit" class="btn btn-success"">送信</button>
+                                </div>
+                            </form>
+                        </div>
+                    </nav>
+                    <div class="text-center">
+                        <a href="{{ route('password.request') }}">パスワードの変更はこちらから</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 @endsection

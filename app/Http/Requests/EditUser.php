965ddb278ddca4f92b73f8e2858ea3889668050f
tomomisugawara <hello.tra.sugawara@gmail.com>
2022-08-19 09:26:52 +0900

    8/19

--- app/Http/Requests/EditUser.php
+++ app/Http/Requests/EditUser.php
@@ -27,7 +27,7 @@ class EditUser extends FormRequest
             'name' => 'required|string|max:255',
             'email' => 'required|string|email:filter|max:255',
 
-            'image' => 'image|mimes:jpeg,png,jpg|max:2048',//8/18追加
+            'image' => 'image|mimes:jpeg,png,jpg|max:9999',//8/18追加
         ];
     }
 
